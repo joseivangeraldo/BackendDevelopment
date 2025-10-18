746940ef0961134ad30c9b9fc79ff874a5b4a3d5
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-10-18 20:35:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello Node js");
+
+const array = [1,2,3,4];
+
+console.log(array, 'array');
+
+setTimeout(() => {
+console.log('This message is delayed by 2 seconds')
+}, 2000);
+
+console.log('This is the last line of the code');
